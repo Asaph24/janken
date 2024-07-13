5fb9f3f509373061764b3465fc88d6f4473d586c
unknown <asaphmateus01@gmail.com>
2024-07-13 17:01:02 -0300

    Added new comments and deleted old ones

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 
+// Keeping tab on the scores //
 let playerScore = 0;
 let computerScore = 0;
 
@@ -8,6 +9,8 @@ let computerScore = 0;
 
 
 function getComputerChoice() {
+
+    // getting a random number between 1 and 3 //
     let math = Math.floor(Math.random() * 3) + 1;
     let computerChoice1;
     if ( math === 1) {
@@ -30,9 +33,13 @@ function getHumanChoice() {
 // playing the round //
 
 function playRound(humanChoice1, computerChoice1) {
+    // The prompt by the user always gets to lower case //
     humanChoice1 = humanChoice1.toLowerCase();
+
+// if else to play around all the different choices //
     if (humanChoice1 === "paper" && computerChoice1 === "paper"){
         console.log("It's a tie")
+// added the score to the board //
         playerScore++
         computerScore++
     } else if (humanChoice1 === "paper" && computerChoice1 === "rock") {
@@ -57,23 +64,18 @@ function playRound(humanChoice1, computerChoice1) {
         
 } 
 
-
-// The score of the match //
-
-
-// adding the score //
-
-
-
 // Playing the game //
 
 function playJanken () {
+
+// looping the game to play it 5 times //
     for (let i = 0; i < 5; i++) {
         const human = getHumanChoice();
         const computer = getComputerChoice();
         playRound(human, computer);
     }
     
+// showing the score to the players
     console.log("It's over")
     console.log("Player Score:", playerScore)
     console.log("Computer Score", computerScore)
